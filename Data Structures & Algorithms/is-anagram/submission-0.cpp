0bfba10128e923a583304095208066d6027311eb
ASJslayer <89019273+ASJslayer@users.noreply.github.com>
2026-05-07 19:50:12 -0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
+
+        map<char, int> s_map;
+        map<char, int> t_map;
+
+        for (int i = 0; i < s.size(); i++) {
+            s_map[s[i]]++;
+            t_map[t[i]]++;
+        }
+
+        return s_map == t_map;
+    }
+};
